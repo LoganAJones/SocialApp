dbb2e040758da193343ddf15c6398f98202aa66f
Paul Diaz <jonesalogan00@gmail.com>
2023-02-07 18:46:05 -0600

    completed backend

--- server/models/User.js
+++ server/models/User.js
@@ -31,7 +31,7 @@ const UserSchema = new mongoose.Schema(
             default: "",
         },
         friends: {
-            type: array,
+            type: Array,
             default: [],
         },
         location: String,
