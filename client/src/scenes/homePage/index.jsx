630951b16eeda9473fe7f82d449fe00b4f43f673
Logan Jones <44062917+LoganAJones@users.noreply.github.com>
2023-03-29 15:16:09 -0500

    updating login/register

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,7 +1,12 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import Navbar from 'scenes/navbar/index';
 
-export default function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+ const HomePage = () => {
+  return <Box>
+    <Navbar/>
+   </Box>
+
 }
+
+export default HomePage
